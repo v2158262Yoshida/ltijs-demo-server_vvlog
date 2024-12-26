ee2ca23bce32316a89821c02b3c3e4c2cb773382
Ubuntu <ubuntu@ip-172-31-27-207.ec2.internal>
2024-12-26 05:22:19 +0000

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-require('dotenv').config()
-const path = require('path')
-const routes = require('./src/routes')
+require('dotenv').config();
+const path = require('path');
+const routes = require('./src/routes');
+const lti = require('ltijs').Provider;
+const cors = require('cors');
 
-const lti = require('ltijs').Provider
+// CORSの設定
+const corsOptions = {
+    origin: 'http://3.219.14.197/moodle', // Moodleの正しいドメインを指定
+    credentials: true,
+};
 
 // Setup
 lti.setup(process.env.LTI_KEY,
@@ -12,40 +18,45 @@ lti.setup(process.env.LTI_KEY,
   }, {
     staticPath: path.join(__dirname, './public'), // Path to static files
     cookies: {
-      secure: false, // Set secure to true if the testing platform is in a different domain and https is being used
-      sameSite: '' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+      secure: false, // Set secure to true if using https
+      sameSite: '' // Set sameSite to 'None' if testing platform is in a different domain
     },
-    devMode: true // Set DevMode to true if the testing platform is in a different domain and https is not being used
-  })
+    devMode: true, // Set DevMode to true for non-https testing
+    dynamicRegistration: true // Enable dynamic registration
+  });
+
+// CORSとルート設定をlti.setupの後に追加
+lti.app.use(cors(corsOptions));
+lti.app.use(routes);
 
 // When receiving successful LTI launch redirects to app
 lti.onConnect(async (token, req, res) => {
-  return res.sendFile(path.join(__dirname, './public/index.html'))
-})
+  console.log('Headers:', req.headers);
+  console.log('Body:', req.body);
+  return res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 // When receiving deep linking request redirects to deep screen
 lti.onDeepLinking(async (token, req, res) => {
-  return lti.redirect(res, '/deeplink', { newResource: true })
-})
-
-// Setting up routes
-lti.app.use(routes)
+  console.log('Headers:', req.headers);
+  console.log('Body:', req.body);
+  return lti.redirect(res, '/deeplink', { newResource: true });
+});
 
 // Setup function
 const setup = async () => {
-  await lti.deploy({ port: process.env.PORT })
+  // サーバーを0.0.0.0でリッスン
+  await lti.deploy({ port: process.env.PORT, serverHost: '0.0.0.0' });
 
   /**
    * Register platform
    */
-  /* await lti.registerPlatform({
-    url: 'http://localhost/moodle',
-    name: 'Platform',
-    clientId: 'CLIENTID',
-    authenticationEndpoint: 'http://localhost/moodle/mod/lti/auth.php',
-    accesstokenEndpoint: 'http://localhost/moodle/mod/lti/token.php',
-    authConfig: { method: 'JWK_SET', key: 'http://localhost/moodle/mod/lti/certs.php' }
-  }) */
-}
-
-setup()
+  await lti.registerPlatform({
+    url: 'http://3.219.14.197/moodle',
+    name: 'Moodle',
+    clientId: 'GTvYcTLFdTVnlUD', // MoodleのクライアントID
+    authenticationEndpoint: 'http://3.219.14.197/moodle/mod/lti/auth.php',
+    accesstokenEndpoint: 'http://3.219.14.197/moodle/mod/lti/token.php',
+    authConfig: { method: 'JWK_SET', key: 'http://3.219.14.197/moodle/mod/lti/certs.php' }
+  });
+};
